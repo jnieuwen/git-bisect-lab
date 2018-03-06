50df5031ae7547cfe6b9bff96328433883d4a4de
Jeroen van Nieuwenhuizen <jeroen@jeroen.se>
2018-03-06 16:56:58 +0100

    commit 1

---
+++ fib35.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+
+if __name__ == '__main__':
+    n = 35
+
+    if n == 0:
+        print(0)
+        sys.exit()
+
+    if n == 1:
+        print("0,1")
+        sys.exit()
+
+    values = []
+    values.append(0)
+    values.append(1)
+
+
+    while len(values) <= n:
+        values.append(values[-1] + values[-2])
+
+    print(",".join(map(str,values)))
+
+
+
